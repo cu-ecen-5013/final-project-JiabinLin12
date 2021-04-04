f5a54d50d16a4cbe1f1f1e890279d018ede95a3a
Steve Rizor <steve.rizor@colorado.edu>
2021-04-04 13:00:55 -0600

    Fixing LEDControlSvc package

--- base_external/package/LEDControlSvc/LEDControlSvc.mk
+++ base_external/package/LEDControlSvc/LEDControlSvc.mk
@@ -5,16 +5,16 @@
 #
 ##############################################################
 
-LED_CONTROL_SVC_VERSION = 'ee08ea06c5f40d4be2002b2295f8682bda7875ff'
-LED_CONTROL_SVC_SITE = 'git@github.com:cu-ecen-5013/final-project-solnus.git'
-LED_CONTROL_SVC_SITE_METHOD = git
-LED_CONTROL_SVC_GIT_SUBMODULES = YES
+LEDCONTROLSVC_VERSION = 'ee08ea06c5f40d4be2002b2295f8682bda7875ff'
+LEDCONTROLSVC_SITE = 'git@github.com:cu-ecen-5013/final-project-solnus.git'
+LEDCONTROLSVC_SITE_METHOD = git
+LEDCONTROLSVC_GIT_SUBMODULES = YES
 
-define LED_CONTROL_SVC_BUILD_CMDS
+define LEDCONTROLSVC_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) all
 endef
 
-define LED_CONTROL_SVC_INSTALL_TARGET_CMDS
+define LEDCONTROLSVC_INSTALL_TARGET_CMDS
 	$(INSTALL) -d 0755 $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/build/LEDControlSvc $(TARGET_DIR)/usr/bin/
 endef
