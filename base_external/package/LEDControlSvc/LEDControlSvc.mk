27e1d27888dda201de4554af9e07be712cc4c791
Steve Rizor <steve.rizor@colorado.edu>
2021-04-04 20:49:13 -0600

    Getting scons cross compile to work

--- base_external/package/LEDControlSvc/LEDControlSvc.mk
+++ base_external/package/LEDControlSvc/LEDControlSvc.mk
@@ -5,13 +5,14 @@
 #
 ##############################################################
 
-LEDCONTROLSVC_VERSION = 'ee08ea06c5f40d4be2002b2295f8682bda7875ff'
+LEDCONTROLSVC_VERSION = '45ae9e74be22e6451a3285e124857af62a4c038d'
 LEDCONTROLSVC_SITE = 'git@github.com:cu-ecen-5013/final-project-solnus.git'
 LEDCONTROLSVC_SITE_METHOD = git
 LEDCONTROLSVC_GIT_SUBMODULES = YES
+LEDCONTROLSVC_DEPENDENCIES = host-scons
 
 define LEDCONTROLSVC_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) all
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) TOOLCHAIN=$(shell echo $(TARGET_CROSS) | sed 's/.$$//') -C $(@D) all
 endef
 
 define LEDCONTROLSVC_INSTALL_TARGET_CMDS
