5cf8736c6525062298d0c500be1e0db24cc73b07
Steve Rizor <steve.rizor@colorado.edu>
2021-04-10 16:39:56 -0600

    Getting azure to link with our service

--- base_external/package/LEDControlSvc/LEDControlSvc.mk
+++ base_external/package/LEDControlSvc/LEDControlSvc.mk
@@ -5,8 +5,9 @@
 #
 ##############################################################
 
-LEDCONTROLSVC_VERSION = 'efb2859df4e891fe287bc9354c57e0d1f38a6d7a'
+LEDCONTROLSVC_VERSION = '2301e7abb3e4f7cd1e39ece6f00f796fb5590552'
 LEDCONTROLSVC_SITE = 'git@github.com:cu-ecen-5013/final-project-solnus.git'
+#LEDCONTROLSVC_SITE = '/home/solnus/class/ext/final-project-solnus/'
 LEDCONTROLSVC_SITE_METHOD = git
 LEDCONTROLSVC_GIT_SUBMODULES = YES
 LEDCONTROLSVC_DEPENDENCIES = host-scons
@@ -17,9 +18,9 @@ endef
 
 define LEDCONTROLSVC_INSTALL_TARGET_CMDS
 	$(INSTALL) -d 0755 $(TARGET_DIR)/usr/bin
-	$(INSTALL) -m 0755 $(@D)/src/AzureSrc/device-start-stop $(TARGET_DIR)/etc/init.d/S99iotdevice
+	#$(INSTALL) -m 0755 $(@D)/src/AzureSrc/device-start-stop $(TARGET_DIR)/etc/init.d/S99iotdevice
 	$(INSTALL) -m 0755 $(@D)/build/LEDControlSvc $(TARGET_DIR)/usr/bin/
-	$(INSTALL) -m 0755 $(@D)/src/AzureSrc/SendCloudToDeviceMessage.py $(TARGET_DIR)/usr/bin/
+	#$(INSTALL) -m 0755 $(@D)/src/AzureSrc/SendCloudToDeviceMessage.py $(TARGET_DIR)/usr/bin/
 
 endef
 
